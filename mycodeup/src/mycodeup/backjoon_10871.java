e900bba82410f92f30069084a8e3f9cedf31bc38
chiseong kim <dlapdlf1013@gmail.com>
2022-04-20 21:17:04 +0900

    2022-04-20

---
+++ mycodeup/src/mycodeup/backjoon_10871.java
@@ -0,0 +1,42 @@
+package mycodeup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class backjoon_10871 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		//sc.nextLine();
+		
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+		
+		String array = br.readLine();
+		
+		String [] A = array.split(" ");
+		
+		for (int i = 0; i < n; i++) {			
+			if (x == 1) {
+				System.out.println(0);
+				break;
+			}else if(Integer.parseInt(A[i]) < x){
+				System.out.printf("%d ",Integer.parseInt(A[i]));
+			}
+			
+			
+			
+		}
+		
+		
+		
+	}
+
+}
